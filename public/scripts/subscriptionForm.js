fffd5674fad7ab69e1343e599d5936a0441aa2cd
gabriele <brezzagabriele0@gmail.com>
2024-05-31 23:05:01 +0200

    zcx

--- public/scripts/subscriptionForm.js
+++ public/scripts/subscriptionForm.js
@@ -1,14 +1,13 @@
-const paymentMethod = document.getElementById('paymentMethod');
-const payPal = document.getElementById('payPal');
-const stripe = document.getElementById('stripe')
+    const paymentMethod = document.getElementById('paymentMethod');
+    const payPal = document.getElementById('payPal');
+    // const stripe = document.getElementById('stripe')
+    payPal.addEventListener('click', () => paymentMethod.value = 'payPal');
+    // stripe.addEventListener('click', () => paymentMethod.value = 'stripe')
 
-payPal.addEventListener('click', () => paymentMethod.value = 'payPal');
-stripe.addEventListener('click', () => paymentMethod.value = 'stripe')
+    const year = document.getElementById('anno');
+    var oggi = new Date()
+    var centoAnniFa = new Date();
+    centoAnniFa.setFullYear(oggi.getFullYear() - 100);
 
-const year = document.getElementById('anno');
-var oggi = new Date()
-var centoAnniFa = new Date();
-centoAnniFa.setFullYear(oggi.getFullYear() - 100);
-
-year.setAttribute("min", centoAnniFa.getFullYear());
-year.setAttribute("max", (oggi.getFullYear() - 14));
+    year.setAttribute("min", centoAnniFa.getFullYear());
+    year.setAttribute("max", (oggi.getFullYear() - 14));
