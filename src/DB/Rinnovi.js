1e075801b915180e16f61b83cf86aebeca0bffcf
gabriele <brezzagabriele0@gmail.com>
2024-06-26 13:05:09 +0200

    s

--- src/DB/Rinnovi.js
+++ src/DB/Rinnovi.js
@@ -9,7 +9,90 @@ connect.then(() =>{
 });
 
 const RinnoviSchema = new mongoose.Schema({
-
+    nome: {
+        type: String,
+        required: true
+    },
+    cognome: {
+        type: String,
+        required: true
+    }, 
+    cf: {
+        type: String,
+        required: true
+    },
+    contatti: {
+        email: {
+            type: String,
+            required: true
+        },
+        tel: {
+            type: String,
+            required: true
+        }
+    },
+    spedizione: {
+        via: {
+            type: String,
+            required: true
+        },
+        nCivico: {
+            type: String,
+            required: true
+        },
+        cap: {
+            type: String,
+            required: true
+        },
+        comune: {
+            type: String,
+            required: true
+        },
+        provincia: {
+            type: String,
+            required: true
+        }
+    },
+    visita: {
+        data: {
+            type: String,
+            required: true
+        },
+        ora: {
+            type: String,
+            required: true
+        }
+    },
+    nPatente: {
+        type: String,
+        required: true
+    },
+    protocollo: {
+        type: String,
+        required: true
+    },
+    fatture: {
+        data:{
+            type: String,
+            required: false
+        },
+        importo:{
+            type: Number,
+            required: false
+        },
+        emessa:{
+            type: Boolean,
+            required: false
+        },
+    },
+    note: {
+        type: String,
+        required: false
+    },
+    archiviato: {
+        type: Boolean,
+        required: false
+    }
 });
 const Rinnovi = new mongoose.model('Rinnovi', RinnoviSchema);
 
--- src/routes/rinnoviRoute.js
+++ src/routes/rinnoviRoute.js
@@ -17,9 +17,61 @@ router.get('/admin/rinnovi', authenticateJWT, async (req, res) =>{
     res.render('admin/rinnovi/usersPage', {users})
 });
 
-router.get('/admin/rinnovi/findData', authenticateJWT, async (req, res) =>{
-    const users = await rinnovi.find({});
-    res.render('admin/rinnovi/findData', {users})
+router.get('/admin/rinnovi/addUser', authenticateJWT, async (req, res) =>{
+    res.render('admin/rinnovi/addUser')
 });
-
+router.post('/rinnovi/addUser', authenticateJWT, async (req, res) => {
+    const {nome, cognome, cf, email, tel, note, nPatente, protocollo} = req.body;
+    const [via, nCivico, cap, comune, provincia] = req.body.indirizzo.split(',');
+    const spedizione = {
+        via: via.trim(),
+        nCivico: nCivico.trim(),
+        cap: cap.trim(),
+        comune: comune.trim(),
+        provincia: provincia.trim()
+    }
+    const [data, ora] = req.body.visita.split('T');
+    const visita = {
+        data: data,
+        ora: ora
+    }
+    const contatti = {
+        email: email,
+        tel: tel
+    }
+    const saveUser = new rinnovi({
+        "nome": nome.trim(),
+        "cognome": cognome.trim(),
+        "cf": cf.trim(),
+        "contatti": contatti,
+        "spedizione": spedizione,
+        "visita": visita,
+        "nPatente": nPatente.trim(),
+        "protocollo": protocollo.trim(),
+        "note": note
+    });
+        saveUser.save()
+        .then(() => {
+            console.log(`Nuovo utente rinnovi salvato: ${nome} ${cognome}`);
+            return res.redirect('/admin/rinnovi/addUser');
+        })
+        .catch((errore) => {
+            console.error(`Errore durante il salvataggio del nuovo utente rinnovi: ${errore}`);
+            return res.render('errorPage', {error: `errore durante il salvataggio dell'utente`});
+        });
+});
+router.post('/rinnovi/deleteUsers', authenticateJWT, async (req, res)=> {
+    const ids = req.body;
+    try {
+        for (const id of Object.values(ids)) {
+            await rinnovi.deleteOne({"_id": id});
+            console.log(`utente rinnovi ${id} eliminato definitivamente`);
+        }
+        return res.redirect('/admin/rinnovi');
+    } catch (error) {
+        console.error(`errore durante l'eliminazione degli utenti rinnovi: ${error}`);
+        return res.render('errorPage', {error: `errore durante l'eliminazione degli alievi`});
+    }
+    
+});
 module.exports = router;
