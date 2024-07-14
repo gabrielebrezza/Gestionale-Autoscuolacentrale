e75001046c291f2938fdf8538c811c4d4a880c1d
gabriele <brezzagabriele0@gmail.com>
2024-07-14 16:59:39 +0200

    s

--- src/routes/rinnoviRoute.js
+++ src/routes/rinnoviRoute.js
@@ -230,6 +230,7 @@ const fetchBookings = async () => {
 
     const pad = (num) => (num < 10 ? '0' + num : num);
     const currentDateTime = `${now.getUTCFullYear()}-${pad(now.getUTCMonth() + 1)}-${pad(now.getUTCDate())}T${pad(now.getUTCHours())}:00:00+02:00`;
+    console.log(currentDateTime)
     const query = `
     query MarconiBookings {
         bookings(
