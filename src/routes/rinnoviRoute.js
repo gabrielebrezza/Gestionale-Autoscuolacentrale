2371024381bb64f80fb51d9ce754e3ff33caafa9
gabriele <brezzagabriele0@gmail.com>
2024-09-12 19:06:44 +0200

    s

--- src/routes/rinnoviRoute.js
+++ src/routes/rinnoviRoute.js
@@ -254,6 +254,7 @@ const fetchBookings = async () => {
 
                     res.on('end', () => {
                         const response = JSON.parse(body);
+                        console.log(response)
                         if(response.includes('html')) return console.log('errore c\'è un html');
                         if (response.data.authenticateUserWithPassword.sessionToken) {
                             resolve(response.data.authenticateUserWithPassword.sessionToken);
