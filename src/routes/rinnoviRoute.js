ff8689aaa9ef4a61841964b761aca85e7402f57e
persempreio <persempreio5@gmail.com>
2025-03-22 13:43:24 +0100

    Update cron job schedule in rinnovi route to run every hour from 08:00 to 23:00, Monday to Saturday, for user license expiration checks.

--- src/routes/rinnoviRoute.js
+++ src/routes/rinnoviRoute.js
@@ -457,9 +457,7 @@ router.post('/admin/programmaScadenziario', authenticateJWT, async (req, res) =>
 });
 
 const cron = require("node-cron");
-//  * * *
-console.log(new Date())
-cron.schedule(" 05 12 * * *", async () => {
+cron.schedule("0 8-23 * * 1-6", async () => {
     console.log("🔄 Avvio ricerca scadenze patente degli utenti programmati");
     try {
         const schedule = await programmaScadenziario.find();
