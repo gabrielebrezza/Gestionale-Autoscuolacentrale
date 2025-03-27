b2fc27fd8caa8638603195513371bfd84838e72d
persempreio <persempreio5@gmail.com>
2025-03-27 19:19:50 +0100

    s

--- src/utils/portaleAutomobilistaUtils.js
+++ src/utils/portaleAutomobilistaUtils.js
@@ -82,7 +82,7 @@ async function searchUserPortale(cf, cognome, nPatente) {
     let browser;
     try {
         browser = await puppeteer.launch({ 
-            headless: false,
+            headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const credenziali = await Credentials.findOne();
@@ -178,7 +178,7 @@ async function searchUserPortale(cf, cognome, nPatente) {
     let browser, totalErrors = 0;
     try {
         browser = await puppeteer.launch({ 
-            headless: false,
+            headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const credenziali = await Credentials.findOne();
