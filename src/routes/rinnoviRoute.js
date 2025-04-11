093dcdb050f078a444fe86ebed4843627776cf20
persempreio <persempreio5@gmail.com>
2025-04-11 14:59:16 +0200

    Update cron schedule in rinnoviRoute.js to change email notification timing from 15:00 to 14:05 for improved renewal email dispatch.

--- src/routes/rinnoviRoute.js
+++ src/routes/rinnoviRoute.js
@@ -589,7 +589,7 @@ if (process.env.SERVER_URL != 'http://localhost') {
         await trySearchAndUpdate(); // Controlla se può eseguire la ricerca ogni 2 ore
     });
 
-    cron.schedule("0 15 * * *", async () => {
+    cron.schedule("5 14 * * *", async () => {
         console.log('🔄 Eseguo L\'invio delle email per i rinnovi')
         await notifyExpirations();
     });
